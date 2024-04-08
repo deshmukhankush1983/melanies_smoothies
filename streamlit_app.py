cd0e0ea2f44fcbae5b7aeda1f9c45c738e601bd1
deshmukhankush1983 <165218490+deshmukhankush1983@users.noreply.github.com>
2024-04-08 01:16:39 -0500

    Update streamlit_app.py
    added request to get data from fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,10 @@ if ingredients_list :
 
         st.success('Your Smoothie is Ordered',icon="✅")
 
+# New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
+
+
